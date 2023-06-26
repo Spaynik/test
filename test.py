8bc58ded2fd88057a7c6d29843e1506a1d94d696
Spaynik <35546069+Spaynik@users.noreply.github.com>
2023-06-27 03:52:21 +0500

    add test

--- test.py
+++ test.py
@@ -7,13 +7,18 @@ with open('schema.json', 'r') as f:
 
 schema['properties']['name']['pattern'] = '^[A-Z][a-zA-Z0-9]*[0-9]$'
 schema['properties']['name']['minLength'] = 3
+schema['properties']['ip_address'] = {
+    'type': 'string',
+    'pattern': "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+}
 
-with open('data.json') as f:
+with open('data.json', 'r') as f:
     data = json.load(f)
 
-for item in data:
+for i, item in enumerate(data):
+    item["ip_address"] = f"192.168.1.{i + 1}"
     try:
         validate(item, schema)
-        print(f"JSON-данные валидны для {item['name']}")
+        print(f"JSON-данные валидны после изменения для {item['name']}, IP: {item['ip_address']}")
     except ValidationError as e:
-        print(f"JSON-данные невалидны для {item['name']}: {e.message}")
+        print(f"JSON-данные невалидны после изменения для {item['name']}, IP: {item['ip_address']}: {e.message}")
